1c5f5e2b80c714240ba74d4141ad5cbe3f405df6
v_wieszheng(郑市委) <v_wieszheng@tencent.com>
2024-09-06 19:51:43 +0800

    ✨ feat: 增加新功能

--- app/schemas/project/project.py
+++ app/schemas/project/project.py
@@ -17,15 +17,15 @@ class ProjectSchemaBase(BaseModel):
     name: str = Field(..., description="项目名称")
     app: str = Field(..., description="项目所属应用")
     owner: int = Field(..., description="项目拥有者")
-    # private: bool = False
-    # description: str = "这是一个项目简介。"
-    # dingtalk_url: str = None
-    #
-    # @field_validator("name")
-    # def validate_username(cls, value: str):
-    #     if 2 > len(value) > 6:
-    #         raise ValueError("项目名称不能小于2大于6个字符")
-    #     return value
+    private: bool = False
+    description: str = "这是一个项目简介。"
+    dingtalk_url: str = None
+
+    @field_validator("name")
+    def validate_username(cls, value: str):
+        if 2 > len(value) > 6:
+            raise ValueError("项目名称不能小于2大于6个字符")
+        return value
 
 
 class UpdateProjectParam(ProjectSchemaBase):
